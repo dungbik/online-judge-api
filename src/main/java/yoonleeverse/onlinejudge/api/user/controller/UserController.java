a71959c130f24d61e6cdc905bb79b076f0ce9c0f
dungbik <q1w2e30630@gmail.com>
2022-05-15 19:41:15 +0900

    Feat: getCurrentUser API (profile)

---
+++ src/main/java/yoonleeverse/onlinejudge/api/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package yoonleeverse.onlinejudge.api.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yoonleeverse.onlinejudge.api.user.entity.UserEntity;
+import yoonleeverse.onlinejudge.api.user.repository.UserRepository;
+import yoonleeverse.onlinejudge.security.CurrentUser;
+import yoonleeverse.onlinejudge.security.UserPrincipal;
+
+@RestController
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping()
+    @PreAuthorize("isAuthenticated()")
+    public UserEntity getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+
+        return userRepository.findByEmail(userPrincipal.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
---
+++ src/main/java/yoonleeverse/onlinejudge/security/CurrentUser.java
@@ -0,0 +1,13 @@
+package yoonleeverse.onlinejudge.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
