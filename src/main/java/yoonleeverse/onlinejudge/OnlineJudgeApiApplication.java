4c3e9c6748f3b24b541062d9094599e8b7fd65cb
dungbik <q1w2e30630@gmail.com>
2022-05-15 17:44:04 +0900

    Feat: Initial Commit

---
+++ src/main/java/yoonleeverse/onlinejudge/OnlineJudgeApiApplication.java
@@ -0,0 +1,13 @@
+package yoonleeverse.onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineJudgeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yoonleeverse/onlinejudge/OnlineJudgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yoonleeverse.onlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJudgeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
