0eab89f8e4cac89e1140929dd00b246f7cdff621
dungbik <q1w2e30630@gmail.com>
2022-07-16 13:40:39 +0900

    Feat: Swagger, Redoc (ApiDoc) Set Up

---
+++ src/main/java/yoonleeverse/onlinejudge/config/OpenAPIConfig.java
@@ -0,0 +1,29 @@
+package yoonleeverse.onlinejudge.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Online Judge API Doc")
+                        .version("0.0.1")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("Bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("Bearer")
+                        )
+                );
+    }
+
+}
