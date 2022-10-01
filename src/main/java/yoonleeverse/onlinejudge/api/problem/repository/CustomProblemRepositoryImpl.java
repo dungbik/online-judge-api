445323a9e89bff53e5e557d4bd4ca8cfeaaf8d9b
dungbik <q1w2e30630@gmail.com>
2022-10-01 17:10:43 +0900

    Feat: getAllProblem API

--- src/main/java/yoonleeverse/onlinejudge/api/problem/repository/CustomProblemRepositoryImpl.java
+++ src/main/java/yoonleeverse/onlinejudge/api/problem/repository/CustomProblemRepositoryImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
 import yoonleeverse.onlinejudge.api.problem.dto.GetAllProblemRequest;
 import yoonleeverse.onlinejudge.api.problem.entity.Problem;
+import yoonleeverse.onlinejudge.util.NumberUtil;
 
 import java.util.List;
 
@@ -24,13 +25,13 @@ public class CustomProblemRepositoryImpl implements CustomProblemRepository {
     @Override
     public Page<Problem> getAllProblem(GetAllProblemRequest req) {
         String title = req.getTitle();
-        int page = req.getPage() == null ? 0 : (req.getPage() - 1);
+        int page = NumberUtil.toPage(req.getPage());
 
         Pageable pageable = PageRequest.of(page, PROBLEM_MAX_SIZE, Sort.by(Sort.Direction.DESC, "id"));
 
         Criteria criteria = new Criteria();
         if (title != null) {
-            criteria.andOperator(Criteria.where("title").regex(".*" + title + "*.", "i"));
+            criteria.andOperator(Criteria.where("title").regex(title, "i"));
         }
 
         Query query = Query.query(criteria);
---
+++ src/main/java/yoonleeverse/onlinejudge/util/NumberUtil.java
@@ -0,0 +1,8 @@
+package yoonleeverse.onlinejudge.util;
+
+public class NumberUtil {
+
+    public static int toPage(Integer value) {
+        return value == null || value < 0 ? 0 : value - 1;
+    }
+}
