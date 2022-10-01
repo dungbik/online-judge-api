e48b2a9d04ac36ad17f7989e1370446aeae9fde2
dungbik <q1w2e30630@gmail.com>
2022-10-01 17:38:21 +0900

    Feat: getAllProblem API

--- src/main/java/yoonleeverse/onlinejudge/api/problem/dto/GetAllProblemRequest.java
+++ src/main/java/yoonleeverse/onlinejudge/api/problem/dto/GetAllProblemRequest.java
@@ -3,9 +3,14 @@ package yoonleeverse.onlinejudge.api.problem.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import yoonleeverse.onlinejudge.api.common.dto.PagingRequest;
+import yoonleeverse.onlinejudge.api.problem.entity.ProgrammingLanguage;
+
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class GetAllProblemRequest extends PagingRequest {
     private String title;
+    private List<ProgrammingLanguage> languages;
+    private List<Long> tags;
 }
--- src/main/java/yoonleeverse/onlinejudge/api/problem/repository/CustomProblemRepositoryImpl.java
+++ src/main/java/yoonleeverse/onlinejudge/api/problem/repository/CustomProblemRepositoryImpl.java
@@ -1,6 +1,7 @@
 package yoonleeverse.onlinejudge.api.problem.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +10,15 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.CollectionUtils;
 import yoonleeverse.onlinejudge.api.problem.dto.GetAllProblemRequest;
 import yoonleeverse.onlinejudge.api.problem.entity.Problem;
+import yoonleeverse.onlinejudge.api.problem.entity.ProgrammingLanguage;
+import yoonleeverse.onlinejudge.api.problem.entity.Tag;
 import yoonleeverse.onlinejudge.util.NumberUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static yoonleeverse.onlinejudge.api.common.constant.Constants.PROBLEM_MAX_SIZE;
 
@@ -25,17 +30,23 @@ public class CustomProblemRepositoryImpl implements CustomProblemRepository {
     @Override
     public Page<Problem> getAllProblem(GetAllProblemRequest req) {
         String title = req.getTitle();
+        List<ProgrammingLanguage> languages = req.getLanguages();
+        List<Long> tags = req.getTags();
         int page = NumberUtil.toPage(req.getPage());
 
         Pageable pageable = PageRequest.of(page, PROBLEM_MAX_SIZE, Sort.by(Sort.Direction.DESC, "id"));
-
         Criteria criteria = new Criteria();
-        if (title != null) {
-            criteria.andOperator(Criteria.where("title").regex(title, "i"));
+        if (StringUtils.isNotEmpty(title)) {
+            criteria.and("title").regex(title, "i");
+        }
+        if (!CollectionUtils.isEmpty(languages)) {
+            criteria.and("languages").all(languages);
+        }
+        if (!CollectionUtils.isEmpty(tags)) {
+            criteria.and("tags").in(tags.stream().map((id) -> Tag.builder().id(id).build()).collect(Collectors.toList()));
         }
 
-        Query query = Query.query(criteria);
-        query.with(pageable);
+        Query query = Query.query(criteria).with(pageable);
 
         List<Problem> problems = this.mongoTemplate.find(query, Problem.class);
         Page<Problem> problemPage = PageableExecutionUtils.getPage(
