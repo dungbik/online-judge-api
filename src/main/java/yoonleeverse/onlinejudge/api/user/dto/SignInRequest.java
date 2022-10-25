b69afce6b37e8651439ed4b61609bcccfe0851ee
dungbik <q1w2e30630@gmail.com>
2022-10-25 20:34:26 +0900

    Feat: 로그인 id, password size check 제거

--- src/main/java/yoonleeverse/onlinejudge/api/user/dto/SignInRequest.java
+++ src/main/java/yoonleeverse/onlinejudge/api/user/dto/SignInRequest.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Size;
 public class SignInRequest {
 
     @Schema(example = "test1234", description = "아이디")
-    @Size(min = 5, max = 20)
+//    @Size(min = 5, max = 20)
     private String id;
 
     @Schema(example = "test1234", description = "비밀번호")
-    @Size(min = 6, max = 20)
+//    @Size(min = 6, max = 20)
     private String password;
 
     @Schema(example = "e5410da7-e23a-4409-8d13-57531eb26332", description = "소셜 계정 로그인 키")
