4f645eccccb07d2fd3544caa437b91e4fda46bfd
dungbik <q1w2e30630@gmail.com>
2022-11-13 19:25:08 +0900

    Feat: AccessDeniedException 처리 추가

--- src/main/java/yoonleeverse/onlinejudge/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/yoonleeverse/onlinejudge/api/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package yoonleeverse.onlinejudge.api.common.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,6 +9,15 @@ import yoonleeverse.onlinejudge.api.common.dto.APIResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public APIResponse onAccessDenied(AccessDeniedException e) {
+        APIResponse response = new APIResponse();
+        response.setSuccess(false);
+        response.setErrMsg(e.getMessage());
+        return response;
+    }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
