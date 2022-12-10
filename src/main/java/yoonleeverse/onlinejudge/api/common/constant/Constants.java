bf07699e64c85d58a9e71e0af7c567c38a6260a3
dungbik <q1w2e30630@gmail.com>
2022-12-10 14:38:35 +0900

    Feat: Pagination Elem Size 수정

--- src/main/java/yoonleeverse/onlinejudge/api/common/constant/Constants.java
+++ src/main/java/yoonleeverse/onlinejudge/api/common/constant/Constants.java
@@ -2,8 +2,8 @@ package yoonleeverse.onlinejudge.api.common.constant;
 
 public class Constants {
 
-    public static final int PROBLEM_MAX_SIZE = 20;
-    public static final int SUBMISSION_MAX_SIZE = 20;
+    public static final int PROBLEM_MAX_SIZE = 10;
+    public static final int SUBMISSION_MAX_SIZE = 10;
 
     public enum ERole {
         ROLE_USER,
